acc07759b1ce673ae4298d01e0f5dafd0fc9fb41
hackett91 <C16723031@mydit.ie>
2019-12-12 06:02:55 +0000

    pwa strategy

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,18 +3,26 @@ import { Component } from '@angular/core';
 import { Platform } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+import { SwUpdate } from '@angular/service-worker';
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
   styleUrls: ['app.component.scss']
 })
 export class AppComponent {
+
+  update: boolean = false;
+
   constructor(
     private platform: Platform,
     private splashScreen: SplashScreen,
-    private statusBar: StatusBar
+    private statusBar: StatusBar,
+    updates: SwUpdate
   ) {
+    updates.available.subscribe(event => {
+      updates.activateUpdate().then(() => document.location.reload());
+
+    });
     this.initializeApp();
   }
 
